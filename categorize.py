b969d07eb26f9d53c96e8efc5a0f99eae9d6b184
helloqiu <mail@helloqiu.pro>
2020-02-25 13:04:36 +0000

    feat: add progress bar

--- categorize.py
+++ categorize.py
@@ -1,6 +1,14 @@
 import json
 import operator
 from emotion_lexicon import emolex_words
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+from utils import get_config
+import nltk
+import progressbar
+
+TEXT_SCORE = 0.5
+TAG_SCORE = 1.0
 
 TAGS = {
     'anger': 'angry',
@@ -25,29 +33,50 @@ EMOTIONS = [
     'angry',
 ]
 
-with open('tweet.json', 'r') as f:
-    tweets = json.loads(f.read())
+config = get_config()
 
-count = {emotion: 0 for emotion in EMOTIONS}
+uri = "mongodb://%s:%s@%s" % (quote_plus(
+    config['mongo_username']), quote_plus(
+        config['mongo_password']), quote_plus(config['mongo_host']))
 
-for tweet in tweets:
-    weight = {emotion: 0 for emotion in EMOTIONS}
-    tags = [t['text'].lower() for t in tweet['entities']['hashtags']]
-    for tag in tags:
-        tag = tag.lower()
-        word = emolex_words[emolex_words.word == tag]
-        if word.empty:
+client = MongoClient(uri)
+db = client.webscience_course_work
+processed_tweet = db.processed_tweet
+marked_tweet = db.marked_tweet
+
+count = processed_tweet.estimated_document_count()
+i = 0
+
+with progressbar.ProgressBar(max_value=count, redirect_stdout=True) as bar:
+    for tweet in processed_tweet.find():
+        bar.update(i)
+        i += 1
+        for e in EMOTIONS:
+            weight = {emotion: 0 for emotion in EMOTIONS}
+        # marking based on text content
+        content = tweet['processed_text']
+        words = nltk.word_tokenize(content)
+        for word in words:
+            result = emolex_words[emolex_words.word == word]
+            if result.empty:
+                continue
+            for e in NRC_EMOTIONS:
+                weight[TAGS[e]] += int(getattr(result, e)) * TEXT_SCORE
+        # marking based on tags
+        if 'entities' in tweet.keys() and 'hashtags' in tweet['entities']:
+            tags = [t['text'].lower() for t in tweet['entities']['hashtags']]
+            for tag in tags:
+                tag = tag.lower()
+                word = emolex_words[emolex_words.word == tag]
+                if word.empty:
+                    continue
+                for e in NRC_EMOTIONS:
+                    weight[TAGS[e]] += int(getattr(word, e)) * TAG_SCORE
+
+        max_emotion = max(weight.items(), key=operator.itemgetter(1))[0]
+        tweet['emotions_weight'] = weight
+        tweet['emotion'] = max_emotion
+        if weight[max_emotion] == 0:
+            # ignore tweets which have 0 marks
             continue
-        for e in NRC_EMOTIONS:
-            weight[TAGS[e]] += int(getattr(word, e))
-    max_emotion = max(weight.items(), key=operator.itemgetter(1))[0]
-    if max(weight.values()) == 0:
-        continue
-    count[max_emotion] += 1
-    print(f'Tweet text: {tweet["text"]}')
-    print(f'Hashtags: {tags}')
-    print(f'Weights: {weight}')
-    print(f'Max Emotion: {max_emotion}')
-    print('=' * 100)
-
-print(count)
+        marked_tweet.insert_one(tweet)
--- pre_process.py
+++ pre_process.py
@@ -2,6 +2,7 @@ from utils import get_config, remove_url, remove_username, translate_emo, correc
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import nltk
+import progressbar
 
 config = get_config()
 
@@ -14,17 +15,23 @@ db = client.webscience_course_work
 raw_tweet = db.raw_tweet
 processed_tweet = db.processed_tweet
 
-for tweet in raw_tweet.find():
-    content = tweet['text']
-    content = content.lower()
-    content = remove_url(content)
-    content = remove_username(content)
-    content = translate_emo(content)
-    content = content.lower().strip()
-    words = nltk.word_tokenize(content)
-    processed_words = []
-    for w in words:
-        processed_words.append(correct_slang(w))
-    processed_text = ' '.join(processed_words)
-    tweet['processed_text'] = processed_text
-    processed_tweet.insert_one(tweet)
+count = raw_tweet.count()
+i = 0
+
+with progressbar.ProgressBar(max_value=count, redirect_stdout=True) as bar:
+    for tweet in raw_tweet.find():
+        bar.update(i)
+        i += 1
+        content = tweet['text']
+        content = content.lower()
+        content = remove_url(content)
+        content = remove_username(content)
+        content = translate_emo(content)
+        content = content.lower().strip()
+        words = nltk.word_tokenize(content)
+        processed_words = []
+        for w in words:
+            processed_words.append(correct_slang(w))
+        processed_text = ' '.join(processed_words)
+        tweet['processed_text'] = processed_text
+        processed_tweet.insert_one(tweet)
